e219534032bb42c9c5c12269082cf9af43a9ceec
Hushberry <Uchegbuvincent4@gmail.com>
2026-07-03 08:28:02 -0500

    Initial TradingBot project setup

---
+++ bot.py
@@ -0,0 +1,7 @@
+import config
+
+print("===========================================")
+print(config.BOT_NAME)
+print(config.VERSION)
+print(config.AUTHOR)
+print("============================================")
---
+++ config.py
@@ -0,0 +1,7 @@
+# ==================================
+# TradingBot Configuration File
+# ==================================
+
+BOT_NAME = "KAI_Bot"
+VERSION = "1.0"
+AUTHOR = "Vincent Chimezirim"
